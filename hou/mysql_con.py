a8e4d21fba5399a2d7a9295db297dc0127bb40e6
Lifangkai <40513619+Lifangkai@users.noreply.github.com>
2020-01-07 11:16:52 +0800

    Delete mysql_con.py

--- hou/mysql_con.py
+++
@@ -1,8 +0,0 @@
-#!/user/bin/env python
-# -*- coding:utf-8 -*-
-import MySQLdb
-import MySQLdb.cursors
-
-db = MySQLdb.connect(host='127.0.0.1', user='root', passwd='123456', db='shop_management',
-                     cursorclass=MySQLdb.cursors.DictCursor)
-db.autocommit(True)
